a12b5126f05b205ff75b0c3684de9165a11668c1
Qua Wilburn <quawilburn@Quas-MacBook-Air.local>
2022-02-08 17:16:44 -0600

    Get specific user by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -13,4 +13,22 @@ router.get("/", async (req, res) => {
   }
 })
 
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const post = await Post.findById(id)
+    if (!post) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" })
+    } else {
+      res.json(post)
+    }
+  } catch {
+    res
+      .status(500)
+      .json({ message: "The post information could not be retrieved" })
+  }
+})
+
 module.exports = router
