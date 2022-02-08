e44d62a095392f54d109f03e2479126be19d14da
Qua Wilburn <quawilburn@Quas-MacBook-Air.local>
2022-02-08 17:06:22 -0600

    Get request to get posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,16 @@
 // implement your posts router here
+const router = require("express").Router()
+const Post = require("./posts-model")
+
+router.get("/", async (req, res) => {
+  try {
+    const post = await Post.find()
+    res.json(post)
+  } catch {
+    res
+      .status(500)
+      .json({ message: "The posts information could not be retrieved" })
+  }
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 // implement your server here
 // require your posts router and connect it here
 const express = require("express")
+const postRouter = require("./posts/posts-router")
 
 const server = express()
 
 server.use(express.json())
+server.use("/api/posts", postRouter)
 
 server.use("*", (req, res) => {
   res.status(404).send(`
