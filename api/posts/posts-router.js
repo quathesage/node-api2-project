ecc5caa48aad85dc33c502299e150dc5699eb784
Qua Wilburn <quawilburn@Quas-MacBook-Air.local>
2022-02-08 17:35:44 -0600

    Put request complete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -49,4 +49,27 @@ router.post("/", async (req, res) => {
   }
 })
 
+router.put("/:id", async (req, res) => {
+  const { id } = req.params
+  const { title, contents } = req.body
+  try {
+    const updatedPost = await Post.update(id, { title, contents })
+    if (!updatedPost) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" })
+    } else if (!title || !contents) {
+      res
+        .status(400)
+        .json({ message: "Please provide title and contents for the post" })
+    } else {
+      res.json(updatedPost)
+    }
+  } catch {
+    res
+      .status(500)
+      .json({ message: "The post information could not be modified" })
+  }
+})
+
 module.exports = router
