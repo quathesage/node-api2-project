4f00866c26f5175f7d4094aa45a402d08a136379
DeQuavion Wilburn <qua.thesage19@gmail.com>
2022-02-08 18:19:36 -0600

    get post comment

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -94,4 +94,23 @@ router.delete("/:id", async (req, res) => {
   }
 })
 
+router.get("/:id/comments", async (req, res) => {
+  const { id } = req.params
+  try {
+    const post = await Post.findById(id)
+    if (!post) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" })
+    } else {
+      const comment = await Post.findPostComments(id)
+      res.json(comment)
+    }
+  } catch {
+    res
+      .status(500)
+      .json({ message: "The comments information could not be retrieved" })
+  }
+})
+
 module.exports = router
